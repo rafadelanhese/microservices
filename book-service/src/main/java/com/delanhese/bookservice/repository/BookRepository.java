ff16c261d56e5436955b50517d88098eddfdb96a
Rafael Delanhese <rafael.delanhese@yahoo.com.br>
2020-10-01 15:43:12 -0300

    Atualização dos métodos

--- book-service/src/main/java/com/delanhese/bookservice/repository/BookRepository.java
+++ book-service/src/main/java/com/delanhese/bookservice/repository/BookRepository.java
@@ -2,12 +2,16 @@ package com.delanhese.bookservice.repository;
 
 import com.delanhese.bookservice.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BookRepository extends MongoRepository<Book, String> {
 
     public List<Book> findAll();
-    public Book findByAuthor(String author);
-    public List<Book> findByTitle(String title);
+    @Query("SELECT * FROM Book book WHERE book.author = :author")
+    public Book findByAuthor(@Param("author") String author);
+    @Query("SELECT * FROM Book book WHERE book.title = :title")
+    public List<Book> findByTitle(@Param("title") String title);
 }
